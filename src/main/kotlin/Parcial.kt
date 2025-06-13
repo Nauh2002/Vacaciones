d64eab147925843b608efdeee10b0ac7a32da4d1
Nauh2002 <ngarcia@estudiantes.unsam.edu.ar>
2025-06-12 19:52:29 -0600

    correcciones

--- src/main/kotlin/Parcial.kt
+++ src/main/kotlin/Parcial.kt
@@ -10,14 +10,11 @@ import java.time.LocalDate
 // pero permitimos que algunas otras, específicas, sean personalizables en clases hijas.
 //
 // Template esDivertido()
-// template tieneCantidadParDeLetras()
 // Primitiva criterioEspecifico()
 // Primitiva esTranquilo
 abstract class Lugar(val nombre: String, val codigoAfip: String){
 
-    open fun esDivertido(): Boolean = tieneCantidadParDeLetras() && criterioEspecifico()
-
-    open fun tieneCantidadParDeLetras(): Boolean = nombre.length % 2 == 0
+    open fun esDivertido(): Boolean = nombre.length % 2 == 0 && criterioEspecifico()
 
     abstract fun criterioEspecifico(): Boolean
 
@@ -27,9 +24,9 @@ abstract class Lugar(val nombre: String, val codigoAfip: String){
 class Ciudad(nombre: String,
              codigoAfip: String,
              var cantidadDeHabitantes : Int,
-             var atraccionesTuristicas: List<String>,
-             var decibelesPromedio: Double): Lugar(nombre, codigoAfip){
-
+             var decibelesPromedio: Double): Lugar(nombre, codigoAfip)
+{
+    val atraccionesTuristicas= mutableListOf<String>()
 
     override fun criterioEspecifico() = atraccionesTuristicas.size > 3 && cantidadDeHabitantes > 100000
 
@@ -42,7 +39,8 @@ class Pueblo(nombre: String,
              codigoAfip: String,
              val extesionKm : Double,
              val fundacion: LocalDate,
-             val provincia: String): Lugar(nombre, codigoAfip){
+             val provincia: String): Lugar(nombre, codigoAfip)
+{
     val litorales = listOf<String>("Entre Ríos","Corrientes","Misiones")
 
     override fun criterioEspecifico() = fundacion.year < 1800 || litorales.contains(provincia)
@@ -59,14 +57,14 @@ class Balnearios(nombre: String,
 
     override fun criterioEspecifico(): Boolean = promedioDeMetrosPlaya > 300 &&  marEsPeligroso
 
-    override fun esTranquilo() = !tienePeatonal
+    override fun esTranquilo(): Boolean = !tienePeatonal
 }
 
 
 // Punto 2
 class Persona{
     var presupuesto: Double = 0.0
-    lateinit var email: String
+    lateinit var mail: String
     lateinit var preferenciaVacaciones: Preferencia
     lateinit var dni: String
 
@@ -83,7 +81,7 @@ interface Preferencia{
     fun esAdecuado(lugar: Lugar): Boolean
 }
 
-object Tranquilidad : Preferencia{
+object Tranquilo : Preferencia{
     override fun esAdecuado(lugar: Lugar) = lugar.esTranquilo()
 }
 
@@ -92,17 +90,21 @@ object Divertido : Preferencia{
 }
 
 
-class Alternador(var proxima: Preferencia, var otra: Preferencia): Preferencia {
+class Bipolar: Preferencia{
+    val preferenciasARotar = mutableListOf<Preferencia>(Tranquilo,Divertido)
+
     override fun esAdecuado(lugar: Lugar): Boolean {
-        val resultado = proxima.esAdecuado(lugar)
-        intercambiar()
+        val primero = preferenciasARotar.first()
+        val resultado = primero.esAdecuado(lugar)
+
+        if (resultado) {rotarPrefencias()}
+
         return resultado
     }
 
-    fun intercambiar() {
-        val temp = proxima
-        proxima = otra
-        otra = temp
+    fun rotarPrefencias(){
+        val primero = preferenciasARotar.removeAt(0)
+        preferenciasARotar.add(primero)
     }
 }
 
@@ -119,50 +121,70 @@ class Combinado : Preferencia{
 
 class Tour(val fechaDeSalida: LocalDate,
            val cantidadDePersonasRequeridas: Int,
-           val lugares: MutableList<Lugar> = mutableListOf(),
            val montoApagarPorPersona: Double)
 {
+    val lugaresAvisitar: MutableList<Lugar> = mutableListOf()
     val personasAviajar = mutableListOf<Persona>()
-    val pendientes = mutableListOf<Persona>()
-    val tourConfirmadoObserver = mutableListOf<TourConfirmadoObserver>()
-    val observerAfip = mutableListOf<ObserverAfip>()
+    var confirmado: Boolean = false
 
+    fun estaLleno(): Boolean = personasAviajar.size >= cantidadDePersonasRequeridas
 
-    fun puedeIr(persona: Persona): Boolean = persona.presupuesto >= montoApagarPorPersona && lugares.all{ lugar -> persona.lugarEsAdecuado(lugar) }
-
-    fun agregarPersonaAlTour(persona: Persona) {
-        if (personasAviajar.size < cantidadDePersonasRequeridas && puedeIr(persona)) {
-            personasAviajar.add(persona)
-        } else {
-            pendientes.add(persona)
-        }
+    fun confirmarTour(){
+        confirmado = true
     }
 
-    fun agregarObserver(observer: TourConfirmadoObserver) {
-        tourConfirmadoObserver.add(observer)
+    fun agregarPersonaATour(persona:Persona){
+        if (!confirmado){ personasAviajar.add(persona) }
+        if (estaLleno()){ confirmarTour() }
     }
 
-    fun confirmarTour(administrador: Administrador) {
-        if (administrador.estaConfirmado(this)) {
-            personasAviajar.forEach { persona ->
-                tourConfirmadoObserver.forEach { it.notificarParticipante(persona) }
-                observerAfip.forEach { it.informarAfip(persona, this) }
-            }
-        }
+    fun quitarPersonaSinCondiciones(persona: Persona){
+        personasAviajar.remove(persona)
+
     }
+
+    fun mailsPersonasAnotadas(): List<String> {
+        return personasAviajar.map { it.mail }
+    }
+
+
 }
 
 class Administrador{
+    val toursADesignar = mutableListOf<Tour>()
+    val personasParaAsignarTour = mutableListOf<Persona>()
+    val personasPendientesDeAsignarTour = mutableListOf<Persona>()
+    val observers = mutableListOf<PostConfirmacionObservers>()
 
-    fun adminAgregarPersona(persona: Persona, tour: Tour) {
-        tour.personasAviajar.add(persona)
+    fun otorgarTourAPersonas() {
+        personasParaAsignarTour.forEach { persona ->
+            toursADesignar
+                .filter { !it.confirmado }
+                .filter { it.montoApagarPorPersona <= persona.presupuesto }
+                .find { it.lugaresAvisitar.all { lugar -> persona.lugarEsAdecuado(lugar) } }
+                ?.also { agregarPersonaATour(it, persona) }
+                ?: run { agregarPersonaAPendientesDeAsignar(persona) }
+        }
+
     }
 
-    fun adminEliminarPersona(persona: Persona,  tour: Tour) {
-        tour.personasAviajar.remove(persona)
+    fun agregarPersonaATour(tour:Tour, persona:Persona){
+        tour.agregarPersonaATour(persona)
+        personasParaAsignarTour.remove(persona)
     }
 
-    fun estaConfirmado(tour: Tour): Boolean = tour.personasAviajar.size == tour.cantidadDePersonasRequeridas
+    fun agregarPersonaAPendientesDeAsignar(persona: Persona){
+        personasPendientesDeAsignarTour.add(persona)
+    }
+
+    fun quitarPersonaDeTour(tour:Tour, persona:Persona){
+        tour.quitarPersonaSinCondiciones(persona)
+    }
+
+    fun confirmarTour(tour: Tour){
+        tour.confirmarTour()
+        observers.forEach { it.ejecutar(tour)}
+    }
 }
 
 
@@ -170,151 +192,57 @@ interface ServicioMail {
     fun enviarMail(mail: Mail)
 }
 
-data class Mail(val from: String, val fechaDeSalida: LocalDate, var fechaLimite: LocalDate, val lugaresAvisitar: List<Lugar>)
+data class Mail(val from: String, val to: String, val subject: String, val content: String)
 
-interface TourConfirmadoObserver {
-    fun notificarParticipante(persona: Persona){
+interface PostConfirmacionObservers {
+    fun ejecutar(tour: Tour){
+
     }
 }
 
-class NotificarPersonas : TourConfirmadoObserver {
-    lateinit var servicioMail : ServicioMail
-    lateinit var fechaDeSalida: LocalDate
-    lateinit var lugaresAvisitar: List<Lugar>
+class EnviarMail: PostConfirmacionObservers {
+    lateinit var servicioMail: ServicioMail
 
-    override fun notificarParticipante(persona: Persona) {
-        val fechaLimite = minOf(fechaDeSalida.minusDays(30), LocalDate.now())
-        servicioMail.enviarMail(Mail(
-            from = persona.email,
-            fechaDeSalida = fechaDeSalida,
-            fechaLimite = fechaLimite,
-            lugaresAvisitar = lugaresAvisitar
-        ))
-    }
-}
-
-
-interface AfipEnviar{
-    fun notificarAfip(data: Data)
-}
-
-data class Data(val from: String, val to: String, val dni: String)
-
-
-interface ObserverAfip{
-    fun informarAfip(persona: Persona, tour: Tour)
-}
-
-
-class InformarAfip : ObserverAfip{
-    lateinit var afipEnviar : AfipEnviar
-    override fun informarAfip(persona: Persona, tour: Tour) {
-        if(tour.montoApagarPorPersona > 10_000_000){
-            afipEnviar.notificarAfip(Data(
-                from = persona.email,
-                to = "AFIP",
-                dni = persona.dni
-            ))
+    override fun ejecutar(tour: Tour) {
+        val fechaLimite = tour.fechaDeSalida.minusDays(30)
+        tour.mailsPersonasAnotadas().forEach {
+            servicioMail.enviarMail(
+                Mail(
+                    from = "admin@admin.com",
+                    to = it,
+                    subject = "El tour ha sido confirmado!",
+                    content = "La fecha de salida será el día ${tour.fechaDeSalida}" +
+                            "La fecha límite de pago será el día $fechaLimite" +
+                            "Visitaremos los siguientes lugares:" +
+                            lugaresAVisitarEnMail(tour)
+                )
+            )
         }
+    }
 
+    fun lugaresAVisitarEnMail(tour: Tour): String {
+        return tour.lugaresAvisitar.joinToString(", ") { it.nombre }
     }
 }
 
-class AlternadorCambioPreferencia : TourConfirmadoObserver {
-    override fun notificarParticipante(persona: Persona) {
-        if (persona.preferenciaVacaciones is Alternador) {
-            val alternador = persona.preferenciaVacaciones as Alternador
-            alternador.intercambiar()
+interface AfipEnviar {
+    fun notificarAfip(data: InterfazAFIP)
+}
+
+data class InterfazAFIP(val from: String, val to: String, val dni: String)
+
+
+class RotadorDePreferenciaBipolar : PostConfirmacionObservers {
+    override fun ejecutar(tour: Tour) {
+        val personas = tour.personasAviajar
+
+        personas.forEach { persona ->
+            val preferencia = persona.preferenciaVacaciones
+
+            if (preferencia is Bipolar) {
+
+                preferencia.rotarPrefencias()
+            }
         }
     }
 }
-
-
-/*Observaciones
-*
-* ---------------------------------------------------
-* Tipos de ideas posibles para resolver los preferencia para irse de vacaciones:
-*
-* 1-Strategy (Lo implementado)
-* Buscamos dinamismo, generando cambios durante la ejecución
-* Desacopla comportamientos a una clase (que es la beneficiada)
-* Es más simple agregar otro elemento a la estructura
-*
-* 2-Crear subclases
-* Separa comportamientos en clases hijas (Solo una herencia por clase)
-* Útil cuando las variantes son pocas, estables y bien definidas (Lugares)
-* Poca flexibilidad, implica nueva clase o subclases
-*
-* 3-Tener variables y condicionales
-* También otorga dinamismo: tener una variable con un string y condicionales me da la misma posibilidad
-* que el strategy
-* El problema es que acopla la lógica a una calse concreta con if y when
-* Útil cuando se necesitan soluciones rápidas y/o con pocos casos
-*
-* ---------------------------------------------------
-* En cuanto al criterio de Strategy (dependiendo de si quiero asignar algún valor)
-* -Atenti que usa object y clases dependiendo del tipo de State que aplica-
-*
-* 1- Stateless
-* Reutilizable como object
-*
-* 2- State full
-* Se utilizan clases ya que cada asignación necesitara de un object independiente
-*
-*-----------------------------------------------------
-* Ventajas en cuanto al armar un Strategy con Interface sobre Abstract Class
-* No pierdo la posibilidad de subclasificar la clase (no gasto la bala de la clase)
-*
-* -----------------------------------------------------
-* Cuando va Strategy?
-* Cuando necesito separar de mi objeto ese algoritmo que encapsulo en objetos polimórficos
-*
-* Cuando va Template Method?
-* Necesito tener objetos polimórficos en una jerarquía que no va a cambiar
-* por lo que no me interesa modificar la identidad del objeto a futuro
-*
-* -----------------------------------------------------
-*
-* Abstract Class
-*
-* 1-Uso de super
-* Problema si al hacer una nueva clase y no se carga, puede generar problemas en la lógica
-*
-* 2-Metodo abstracto
-* Me obliga a cargar el metodo necesario al crear nuevas clases
-* Se usa porque así lo dice el Dodain, sino, pal lobby
-*
-* -----------------------------------------------------
-* Inmutabilidad
-*
-* 1-val
-* Al generar objeto con atributos con "val" se generan objetos del tipo inmutables
-* Es una decisión de diseño que implica que no queremos que ese atributo sea modificado
-* Se puede leer, pero no se puede volver a asignar
-* En una colección, podra ser mutable su contenido, pero no la referencia (el tipo de objetos que guardo).
-*
-* 2-var
-* El atributo puede ser reemplazado con otra instancia o valor en cualquier momento
-* Se puede leer y escribir
-* Menos seguro cuando se busca inmutabilidad
-*
-* -----------------------------------------------------
-* Observer
-*
-* Uso de Long Parameter Method
-* Al usar varios campos de otros objetos como la data class, se puede estructurar de una forma más ordenada el contenido que tendría un mail por ej
-* Como no están agrupados los atributos que tienen la data class, ayuda a un mejor uso
-*
-* Data class
-* Se tiene por default metodos como equals, copy... permiten testearlos más fácil
-* No necesitan getters ni setters
-* Permiten representar los objetos que vamos a vincular con las APIs
-* Inmutables por defecto
-*
-* Value Object
-* Como el data class de mail, que lo usamos para representar el concepto de un mail
-*
-*/
-
-
-
